52182a3253b32e76a80536a300025bfc92389d2d
Izzy Brown <izzybrown@hotmail.co.uk>
2018-12-29 09:54:51 +0000

    syntax

--- lib/parser.rb
+++ lib/parser.rb
@@ -3,7 +3,7 @@ class Parser
 
   def initialize(path)
     @path = path
-    @entry = Hash.new { |h, k| h[k] = [] }
+    @entry = Hash.new { |hash, key| hash[key] = [] }
   end
 
   def parse
@@ -15,20 +15,20 @@ class Parser
 
   def page_views
     @page_views ||=
-    view_count(unique: false).sort_by { |h, k| k.size }.to_h
+    view_count(unique: false).sort_by { |hash, key| key.size }.to_h
     @page_views
   end
 
   def unique_views
     @unique_views ||=
-    view_count(unique: true).sort_by { |h, k| k.size }.to_h
+    view_count(unique: true).sort_by { |hash, key| key.size }.to_h
   end
 
   private
 
   def view_count(unique:)
-    @entry.each_with_object({}) do |(k, v), list|
-    list[k] = unique ? v.length : v.uniq.length
+    @entry.each_with_object({}) do |(key, value), list|
+    list[key] = unique ? value.length : value.uniq.length
     end
   end
 
