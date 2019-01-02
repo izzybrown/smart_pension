c6ce8dfb243c8d36d6d6ce730accfba2691ba532
Izzy Brown <izzybrown@hotmail.co.uk>
2019-01-02 11:10:45 +0000

    finalised tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe Parser do
   describe '#parse' do
     let(:manual_test) do
       {
-        '/contact'   => ['1.1.1.0'],
+        '/contact'   => ['1.1.1.0', '126.318.035.038'],
         '/target' => ['20.130.140.1'],
-        '/home_2'   => ['897.280.786.156'],
+        '/home_2'   => ['897.280.786.156', '655, 909, 543, 566'],
         '/help_page' => ['4.5.6.7'],
-        '/about' => ['66.77.88.9']
+        '/about' => ['66.77.88.9', '456.094,.765.345', '897.280.786.156']
       }
     end
 
@@ -29,15 +29,33 @@ RSpec.describe Parser do
   end
 
   describe '#page_views' do
+    before { subject.parse}
+
+      let(:page_view_results) do
+      {
+        '/about'   => 3,
+        '/target' => 1,
+        '/contact'    => 2,
+        '/home_2'   => 2,
+        '/help_page' => 1
+      }
     it 'should group the visits by the different urls' do
-      expect(subject.page_views).to eq(5)
+      expect(subject.page_views).to eq(page_view_results)
     end
   end
 
 
   describe '#unique_views' do
     it 'should order url views with unique ip count' do
-      expect(subject.unique_views).to eq(5)
+      let(:unique_view_results) do
+        {
+        '/about'   => 3,
+        '/contact'    => 2,
+        '/home_2'   => 2,
+        '/target' => 1,
+        '/help_page' => 1
+        }
+      expect(subject.unique_views).to eq(unique_view_results)
     end
   end
 end
